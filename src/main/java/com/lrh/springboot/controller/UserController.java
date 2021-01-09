51dca2e84b4caf92a2b11d175215959a521c9df2
心无所住 <oncwnuIeCiKvHxinQp2IgL7WDtYQ@git.weixin.qq.com>
2021-01-09 22:31:41 +0800

    add user controller

---
+++ src/main/java/com/lrh/springboot/controller/UserController.java
@@ -0,0 +1,16 @@
+package com.lrh.springboot.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/users")
+@RestController
+public class UserController {
+
+    @GetMapping("/id")
+    public String test(){
+        return "springboot test cicd";
+    }
+
+}
